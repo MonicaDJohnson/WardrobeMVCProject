884bb82cbe66121405d98efe40be793bbeae8b6c
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-07-27 11:41:12 -0400

    initial commit

---
+++ WardrobeMVCProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WardrobeMVCProject.Startup))]
+namespace WardrobeMVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
